318f56dca6697bd2bfca00c0944a8838f327ca78
Rafael Jeremias <rafael.jeremias.vs@outlook.com>
2024-03-28 22:00:30 -0300

    Conversão de Temperatura

--- src/ListaDeExercicios/Exercicio02/Program.cs
+++ src/ListaDeExercicios/Exercicio02/Program.cs
@@ -5,9 +5,20 @@
         static void Main(string[] args)
         {
             /*
-             * Converter a temperatura de graus Fahrenheit para graus Celsius
-             */
-            Console.WriteLine("Hello, World!");
+            Converter a temperatura de graus Fahrenheit para graus Celsius.
+            Exemplo de entrada: Temperatura em Fahrenheit = 68
+            Fórmula: Celsius = (Fahrenheit - 32) * 5 / 9
+            Exemplo de saída: A temperatura em Celsius é 20 °C
+            */
+            
+            Console.Write("Digite a temperatura em Fahrenheit: ");
+
+            double fahrenheit = double.Parse(Console.ReadLine()); 
+            
+            double celsius = (fahrenheit - 32) * 5 / 9;
+                        
+            Console.WriteLine("Temperatura em Celsius: {0}", celsius + "°C");
+            Console.ReadKey();
         }
     }
 }
